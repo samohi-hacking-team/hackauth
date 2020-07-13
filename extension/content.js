2999e765e5948b1633dce21b4c689416db304bce
JacobZwang <zwang.jacob@gmail.com>
2020-07-12 19:43:40 -0700

    yellow warnings

--- extension/content.js
+++ extension/content.js
@@ -5,10 +5,22 @@ var bendata = [
 },
 {
   "flag": "Started before hackathon",
-  "severity": "1"
+  "severity": "0"
 },
   {
-    "flag": "Started before hackathon",
+    "flag": "Started befo hackathon",
+    "severity": "1"
+  }, 
+  {
+    "flag": "Started befo hackathon",
+    "severity": "0"
+  }, 
+  {
+    "flag": "Started befo hackathon",
+    "severity": "0"
+  }, 
+  {
+    "flag": "Started befo hackathon",
     "severity": "1"
   }, 
 
@@ -17,13 +29,18 @@ var bendata = [
 insertFlags(bendata)
 
 function insertFlags(data) {
-  var hackauth = document.getElementById('software-header');
+  const hackauth = document.getElementById('software-header');
 
   for (var i = 0; i < data.length; i++) {
-    var flag = `<div class = "flag">
+    var flag = `<div class = "flag" id="flag-${i}">
     <div class="flagContent">${data[i].flag}</div>
-    <div class="flagContent">${data[i].severity}</div>
   </div>`
     hackauth.insertAdjacentHTML('beforeend', flag);
+    if (data[i].severity == "1") {
+      document.getElementById("flag-" + i.toString()).style.backgroundColor = "yellow";
+    }
+    else {
+      document.getElementById("flag-" + i.toString()).style.backgroundColor = "#fffa99";
+    }
   }
 }
